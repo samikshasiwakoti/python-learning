79962aa70141a6a98b61c6dedf80d694134e5d27
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-16 13:45:10 +0545

    Initial commit-Python learning

---
+++ Chapter 1/problem3.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello, I am a text to speech engine. I can convert your text into speech.")
+engine.runAndWait()
---
+++ Chapter 1/problem4.py
@@ -0,0 +1,10 @@
+import os
+
+directory_path = '/'
+
+contents = os.listdir(directory_path)
+
+# for item in contents:
+#     print(item)
+
+print(contents)
