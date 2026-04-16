3463b241565fbb03753484ba75d5e6aa7a5f60d9
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-16 14:33:04 +0545

    Updated in chapter 2

---
+++ Chapter 2/datatypes.py
@@ -0,0 +1,19 @@
+price = 99.5 # it is float type
+print(price)
+
+age = 30 # it is int type
+print(age)
+
+name = "Sami" # it is string type
+print(name)
+
+names = None # it is NoneType
+print(names)
+
+# Rules
+# A variable name can contain alphabets, digits and underscores.
+# Can only start with an alphabet and underscores.
+# Can’t start with an digit.
+# A variable name can’t start with a digit
+# No while space is allowed to be used inside a variable name.
+
