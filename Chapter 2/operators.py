aa958f53025bf633e7775c04ae6746f78920a30c
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-16 16:23:38 +0545

    Update to chapter 2

---
+++ Chapter 2/operators.py
@@ -0,0 +1,34 @@
+# Airthmetic Operators
+# symbol is operator and value is operand
+a= 34
+b = 4
+c = a + b
+print(c)
+
+#Assignment Operators
+a = 4-2  #Assign 4-2 in a
+print(a)
+b = 6
+b += 3  # it is increment of b by 3
+print(b)
+
+
+# Comparison Operators( it gives value true or false)
+d =5<4
+print(d)
+
+#Logical Operators
+
+e = True or False # in or if any one value is true than in or it will be true
+print(e)
+
+f = True and False # in and if any one value is false than in and it will be false
+print(f)
+
+# not operstor which  is used to make true to false and false is to true
+g = not True
+print(g)
+
+h = not False
+print(h)
+
---
+++ Chapter 2/type.py
@@ -0,0 +1,17 @@
+a = 31
+t = type(a) # it tells it is class int data type
+print(t)
+
+b = 3.14
+t = type(b) # it tells it is class float data type  
+print(t)
+c = "Sami"
+t = type(c) # it tells it is class str data type    
+print(t)
+
+a ="73.5"
+b = float(a)
+ # anything in double or single quote is string data type
+t = type(b) # it tells it is class float data type    
+print(t)
+
