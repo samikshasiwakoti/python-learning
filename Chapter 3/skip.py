e8965b3b0d542fa34689e20fdfe65c8cdba8a2f7
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-16 22:02:41 +0545

    Updated in chapter 3

---
+++ Chapter 3/skip.py
@@ -0,0 +1,7 @@
+name = "abcdefghijklmopre"
+words = name[1:12:2] # means jumping 2 positions each time
+print(words)
+
+name1 = "Ssssaaammiiiksssha"
+words1 = name1[0:14:3] # means jumping 3 positions each time
+print(words1)
