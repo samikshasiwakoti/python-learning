c30bdc00ac44b92f252331a062b58f784d5b1f04
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-16 21:31:54 +0545

    Chapter 3

---
+++ Chapter 3/intro_to_strings.py
@@ -0,0 +1,9 @@
+name1 = 'sami' # Single quoted string
+name2 ="samii" #  double quoted string
+name3 = '''samii''' # Tripple quoted string
+# They all mean same 
+
+nameshort = name1[0:3] # start from index 0 all the way till 3 (exceeding 3 means up to 2 like index 0,1,2 )
+print(nameshort)
+character1 = name1[1]
+print(character1)
