6b15c16cfd78ca054cf24fe69e59c11b5108f9a7
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-16 16:54:08 +0545

    updated in chapter 2

--- Chapter 2/problem1.py
+++ Chapter 2/problem1.py
@@ -1,3 +1,5 @@
+# write a python program to add two numbers
+
 a = 10
 b = 20
 print(a+b)
---
+++ Chapter 2/problem2.py
@@ -0,0 +1,6 @@
+# write a program to find remainder when a number is divided by z
+
+number = 10
+z = input('Enter a number to divide')
+divison = number % int(z)
+print(divison)
