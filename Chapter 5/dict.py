d1e7da4bc65b3ae08b107304e95967fb0fc542bb
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-17 15:27:14 +0545

    Chapter 5 is about dictionary and sets

---
+++ Chapter 5/dict.py
@@ -0,0 +1,8 @@
+
+marks = {
+    "samiksha" : 100,
+    "asmita" : 200,
+    "samii"  : 150
+}
+print(marks,type(marks))
+print(marks["samiksha"])
---
+++ Chapter 5/dict_method.py
@@ -0,0 +1,13 @@
+marks = {
+    "samiksha" : 100,
+    "asmita" : 200,
+    "samii"  : 150,
+    0:"samiksha"
+}
+print(marks.items())
+print(marks.keys())
+marks.update({"samiksha": 170, "asmita": 300})
+print(marks)
+print(marks.get("samii"))
+print(marks.pop('asmita'))
+print(marks.popitem())
