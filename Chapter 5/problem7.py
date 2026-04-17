a8285c8424987725c330e44e1f92b64a8c77a80b
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-17 17:50:24 +0545

    Updated in Chapter 5

---
+++ Chapter 5/problem7.py
@@ -0,0 +1,2 @@
+#if the names of 32 friends are same.What will happe to the program in problem6?
+# If two friends have the same name in your dictionary, then the latest one will replace the previous one.
