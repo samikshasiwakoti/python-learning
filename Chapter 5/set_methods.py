1fd3ac2875758798960718bb29a6a3b735324eca
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-17 16:48:24 +0545

    Updated in chapter 5

---
+++ Chapter 5/set_methods.py
@@ -0,0 +1,20 @@
+s = { 1, 5, 32, 54, 5, 5, 5, "Samii"}
+print(s,type(s))
+s.add(5566)
+print(s,type(s))
+print(len(s))
+s.remove(32)
+print(s)
+
+s1 = {1,2,6,8,9,10}
+s2 = {7,6,1,11,12,13}
+print(s1.union(s2))
+print(s1.intersection(s2))
+
+s3 ={5,7,9,10,11,12}
+print(s3.union({8,11})) # if duplicate is there it will ignored and if new value is there it would add
+print(s3.intersection({7,9}))
+
+s3 = {1, 2, 3}
+
+print(s3.intersection({7, 9})) # it gives empty set
