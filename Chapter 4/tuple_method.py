48108c08ff7c2e556bf03f0e0bb803dabfab5d87
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-17 13:58:25 +0545

    Updated in chapter 4

---
+++ Chapter 4/tuple_method.py
@@ -0,0 +1,20 @@
+fruit = ("apple","bannana","Litchi")
+print(fruit.count("apple"))
+print(fruit.index("Litchi"))
+print("mango" in fruit)
+print(len(fruit))
+print(len("apple"))
+print(fruit[1:3])
+print(fruit[1:])
+
+number = (1 ,2 ,3, 4)
+repeated = number *3
+print(repeated)
+
+print(max(number))
+print(min(number))
+
+#unpacking tuple
+my_tuple = (1,2,3,4)
+a,b,c,d = my_tuple
+print(a,b,c,d)
