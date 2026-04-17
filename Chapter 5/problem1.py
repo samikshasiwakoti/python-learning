a08007d5d8b926e0acf9a47188963fd6f21b33cc
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-17 17:14:47 +0545

    Updated in chapter 5

--- Chapter 5/problem1.py
+++ Chapter 5/problem1.py
@@ -1,8 +1,9 @@
 #Write a program to create a dictionary of nepali words with values as theri English translation.Provide user with an option to look it up
-word = {
+words = {
 
     "namaste": "Hello",
     "dhanyabad" :"Thank you",
     "ghar" : "Home"
 }
-print( "Dictionary:" ,word)
+word = input("enter the word you want meaning of :")
+print(words[word])
