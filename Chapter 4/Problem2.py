9e53594fc4de555b80481fed9525abfa6037921c
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-17 14:25:23 +0545

    Updated in chapter 4

---
+++ Chapter 4/Problem2.py
@@ -0,0 +1,17 @@
+# # write a program to accept marks of students and display them in a sorted manner.
+# marks = [50, 20, 90, 80.5, 77,55,23]
+# marks.sort()
+# print(marks)
+marks =[]
+marks1 = int(input("Enter a marks"))
+marks.append(marks1)
+marks2 = int(input("Enter a marks"))
+marks.append(marks2)
+marks3 = int(input("Enter a marks"))
+marks.append(marks3)
+marks4 = int(input("Enter a marks"))
+marks.append(marks4)
+marks5 = int(input("Enter a marks"))
+marks.append(marks5)
+marks.sort()
+print(marks)
