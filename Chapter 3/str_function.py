aeff81dd1673aee743cbfb061987525968a0c989
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-17 11:21:17 +0545

    Updated in chapter 3

---
+++ Chapter 3/str_function.py
@@ -0,0 +1,16 @@
+name = "Atiff"
+name1 = "atiff"
+print(len(name))
+print(name.endswith("rry"))
+print(name.startswith("ha"))
+print(name.startswith("At"))
+print(name1.capitalize()) # it makes first sentence capital
+
+text="  hello world  "
+print(text.upper())
+
+print(text.strip())
+print(text.split())
+print(text.replace("world","sami"))
+print(text.count("l"))
+print(text.find("e"))
