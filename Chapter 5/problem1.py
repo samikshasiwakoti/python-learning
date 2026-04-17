149289d9bb2d9fff859d404df43939524cc6d826
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-17 17:09:55 +0545

    Updated in chapter 5

---
+++ Chapter 5/problem1.py
@@ -0,0 +1,8 @@
+#Write a program to create a dictionary of nepali words with values as theri English translation.Provide user with an option to look it up
+word = {
+
+    "namaste": "Hello",
+    "dhanyabad" :"Thank you",
+    "ghar" : "Home"
+}
+print( "Dictionary:" ,word)
