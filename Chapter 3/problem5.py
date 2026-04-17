ddb5354f380e99752dacde6fa6cca9f68f8b2a52
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-17 12:25:25 +0545

    Updated in chapter 3

---
+++ Chapter 3/problem5.py
@@ -0,0 +1,4 @@
+# write a problem to format the following letter using escape sequence characters.
+Letter = "Dear samii, this python course is nice.Thanks!"
+Letter1 = "Dear samii,\n\tthis python course is nice\n.Thanks!"
+print(Letter1)
