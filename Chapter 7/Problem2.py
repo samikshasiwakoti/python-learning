79b8395a7c5fe2dffb5ebd406dc2c46b85201d9c
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-18 21:16:29 +0545

    Updated in Chapter 7

---
+++ Chapter 7/Problem2.py
@@ -0,0 +1,5 @@
+# Write a program to greet all the person names stored in a list'l' and which starts with s
+l=['harry', 'soham','sachin','rahul']
+for names in l:
+    if(names.startswith('s')):
+       print(names)
