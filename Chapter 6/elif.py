37fd21d61d224654cf14bddc0d0acb6dec6fcd30
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-18 19:13:42 +0545

    Updated in Chapter 6

---
+++ Chapter 6/elif.py
@@ -0,0 +1,12 @@
+x = int(input("Enter a number"))
+if(x>90):
+    print("You got A+")
+elif(x==80):
+    print("your got A")
+elif(x>70):
+    print("You got B")
+
+elif(x>50):
+    print("you got c+")
+else:
+    print("you are fail")
