4db65fb1150d89b5befa0ea3e78bdc1d1c5d91b4
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-18 21:25:32 +0545

    Updated in Chapter 7

---
+++ Chapter 7/problem3.py
@@ -0,0 +1,13 @@
+# increasing star pattern where n =3
+n = 3
+for i in range(1,n+1):
+    print("*"*i)
+
+    # decreasing star pattern
+    #range(start, stop, step)
+    n = 3
+    for i in range(n,0,-1):
+        print("*" * i )
+
+
+       
---
+++ Chapter 7/problem4.py
@@ -0,0 +1,4 @@
+ #multiplication in reverse order
+n =int(input("Enter a number:"))
+for i in range(10,0,-1):
+    print(f"{n}  x {i} = {n*i}")
