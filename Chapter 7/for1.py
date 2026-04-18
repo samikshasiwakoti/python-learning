5bbf43377a7b4f7bd9621e8d5569abec711c3d03
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-18 20:33:57 +0545

    Updated in Chapter 7

---
+++ Chapter 7/for1.py
@@ -0,0 +1,15 @@
+
+# for loop with list
+l =[1,2,4,6,7,77,98]
+for i  in l:
+    print(i)
+# for loop with string
+    name="Samiksha"
+    for i in name:
+        print(i)
+ # for loops in else
+b = [1,7,8]
+for item in b:
+    print(item)
+else:
+    print("done") # this is printed when the loop exhausts
