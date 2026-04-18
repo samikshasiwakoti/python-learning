bf1939eff30d23112e2ddc1b1a4631e047010328
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-18 20:39:33 +0545

    Updated in Chapter 7

---
+++ Chapter 7/break_continue_pass.py
@@ -0,0 +1,15 @@
+for i in range(2,9):
+  if i == 4:
+    break
+  print(i)
+
+
+for i in range(2,9):
+  if i == 5:
+    continue
+  print(i)
+
+for i in range(2,9):
+  if i == 6:
+    pass
+  print(i)
