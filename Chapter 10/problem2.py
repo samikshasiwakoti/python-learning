1f350f7e918c2645f3653eb22e87dadbe69bd1f7
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-19 17:43:32 +0545

    Updated in Chapter 10

--- Chapter 10/problem2.py
+++ Chapter 10/problem2.py
@@ -4,6 +4,12 @@ class Calculator:
 
     def square(self):
         print(f"The square is {self.n * self.n}")
+    def cube(self):
+        print(f"The cube is {self.n * self.n * self.n}")
+    def squareroot(self):
+        print(f"The squareroot is {self.n**1/2}")
 
 a = Calculator(4)
 a.square()
+a.cube()
+a.squareroot()
