c74454a0b86299068eef5199864848f4dc08cf86
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-19 12:51:25 +0545

    Updated in chapter 8

---
+++ Chapter 8/probelm4.py
@@ -0,0 +1,7 @@
+#Write a recursive function to calculate the sumof first n natural numbers.
+
+def function():
+    n = int(input("Enter a number"))
+    sum = n * (n +1) //2
+    print(f"The sum of first natural number is{sum}")
+function()
