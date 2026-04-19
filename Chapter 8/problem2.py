539e553b49ba532d9d80ce998d5a9701e553a1a2
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-19 12:37:14 +0545

    Updated in Chapter 8

---
+++ Chapter 8/problem2.py
@@ -0,0 +1,8 @@
+# write a program to convert celsisus into fahrenheit
+def conversion():
+   celsius = int(input("Enter temperature in celsisus:"))
+   fahrenheit = (celsius *9/5) + 32
+   print(f"The temperature of {celsius}°C to Fahrenheit is {fahrenheit}")
+conversion()
+
+
