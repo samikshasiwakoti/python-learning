9658ebbb3f367d5abb4d42c9fc859c1d2b07a837
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-19 11:20:04 +0545

    It is about function and recursions in Chapter 8

---
+++ Chapter 8/function.py
@@ -0,0 +1,21 @@
+
+def greet(): # def is used for normal function
+    print("hello")
+greet()
+
+# for one line function we used lambada
+add = lambda a, b: a +b
+print(add(2,3))
+
+def avg():
+    a = int(input("Enter your number"))
+    b = int(input("Enter your number"))
+    c= int(input("Enter your number"))
+
+    average = (a+b+c)/3
+    print(average)
+avg()
+avg()
+avg()
+avg()
+avg()
