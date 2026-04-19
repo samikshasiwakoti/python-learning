00102ed2acf70d16d3c329c8486457e21af89a11
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-19 15:58:41 +0545

    Updated in Chapter 10

---
+++ Chapter 10/class.py
@@ -0,0 +1,12 @@
+class Employee:
+    language = "hindi" # class atrributes
+    salary = 12000
+
+harry = Employee()
+harry.name = "harry" # this is object attribute which is also known as instance atttribute
+print(harry.name, harry.language, harry.salary)
+
+rohan = Employee()
+rohan.name = "Rohan Roro Robinson"
+print(rohan.name, rohan.language, rohan .salary)
+
---
+++ Chapter 10/instance_vs_class.py
@@ -0,0 +1,8 @@
+class Employee:
+    language = "hindi" # class atrributes
+    salary = 12000
+
+harry = Employee()
+harry.language = "Javascript"
+print(harry.language, harry.salary)
+
