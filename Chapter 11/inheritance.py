2973a6de938504f46f8f21d6af72c0291d1be1fc
samiksha siwakoti <siwakotisamiksha592@gmail.com>
2026-04-20 12:34:51 +0545

    Updated in Chapter 10

---
+++ Chapter 11/inheritance.py
@@ -0,0 +1,26 @@
+class Animal:
+    def speak(self):
+        print("ANimal speaks")
+class Dog(Animal) :
+    def bark(self) :
+        print("Dog barks")  
+# class Cat(Animal):
+#     def meows(self):
+#         print("Cat MewMew")   
+# class Cow(Animal) :
+#     def moo(self):
+#         print("Cow Moos")         
+
+a = Dog()       
+a.speak() 
+a.bark()
+
+# b = Cat()
+# b.speak()
+# b. meows()
+
+# c = Cow()
+# c.speak()
+# c.moo()
+
+
